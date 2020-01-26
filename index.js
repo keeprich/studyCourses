58df4c2ed9498c0e6f61d6c4996e59bfcb62cad2
E B <keeprich@es-macbook-air.lan>
2020-01-26 14:02:08 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,105 @@
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+// });
+
+// NAVIGATION START
+function openNav() {
+  document.getElementById("myNav").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.display = "none";
+}
+// NAVIGATION ENDS
+
+// type
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #ffffff}";
+  document.body.appendChild(css);
+};
+
+// WHAT WE OFFER START
+$("#nav a").click(function(e){
+  e.preventDefault();
+  $(".toggle").hide();
+  var toShow = $(this).attr('href');
+  $(toShow).show();
+});
+// WHAT WE OFFER ENDS
+
+
+
+// COUNTER STARTS
+
+$('.count').each(function () {
+  $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+  }, {
+      duration: 4000,
+      easing: 'swing',
+      step: function (now) {
+          $(this).text(Math.ceil(now));
+      }
+  });
+});
+
+// COUNTER ENDS
+
+
+// FOOTER START
+var d = new Date();
+  var n = d.getUTCFullYear();
+  document.getElementById("demo").innerHTML = n;
+// FOOTER ENDS
---
+++ otherPages/about/about_us.js
@@ -0,0 +1,18 @@
+
+
+// NAVIGATION START
+function openNav() {
+    document.getElementById("myNav").style.display = "block";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+  }
+  // NAVIGATION ENDS
+
+
+  // FOOTER START
+var d = new Date();
+var n = d.getUTCFullYear();
+document.getElementById("demo").innerHTML = n;
+// FOOTER ENDS
